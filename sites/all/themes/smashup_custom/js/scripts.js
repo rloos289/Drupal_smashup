14dd0d964d5481974927a1e509e1a4b4c8412681
Ryan Loos <RLoos289@gmail.com>
2016-12-10 21:13:19 -0800

    set up basic custom theme

---
+++ sites/all/themes/smashup_custom/js/scripts.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($) {
+  $('#site-slogan').click(function(){
+    alert('Congratulations! You found the secret! Take a screenshot of this message and present it at checkout for 50% off your first purchase.');
+  });
+});
